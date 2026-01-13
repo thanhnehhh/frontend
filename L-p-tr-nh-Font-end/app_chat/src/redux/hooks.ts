0efc93893a0be3803b1a4e9bb35c0b2faf0640b1
Trần Ngọc Thanh <22130253@st.hcmuaf.edu.vn>
2026-01-13 18:46:03 +0700

    Delete L-p-tr-nh-Font-end directory

--- L-p-tr-nh-Font-end/app_chat/src/redux/hooks.ts
+++
@@ -1,5 +0,0 @@
-import { useDispatch, useSelector } from "react-redux";
-import type { RootState, AppDispatch } from "./Store";
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector = <T,>(selector: (state: RootState) => T) => useSelector<RootState, T>(selector);
